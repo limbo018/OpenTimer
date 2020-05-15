dd9d60b500fd1e5825746a4e5d21bec497700094
GZZ <gzz_2000@126.com>
2020-05-15 16:28:39 +0000

    Auto Benchmark

--- example/full-profile/fp.cpp
+++ example/full-profile/fp.cpp
@@ -1,97 +1,68 @@
 #include <ot/timer/timer.hpp>
 #include <ot/timer/_prof.hpp>
 
-void i_netcard(ot::Timer &timer) {
-  timer.read_celllib("./benchmark/netcard/netcard_iccad_Early.lib", ot::MIN)
-    .read_celllib("./benchmark/netcard/netcard_iccad_Late.lib", ot::MAX)
-    .read_verilog("./benchmark/netcard/netcard_iccad.v")
-    .read_spef("./benchmark/netcard/netcard_iccad.spef")
-    .read_timing("./benchmark/netcard/netcard_iccad.timing");
+std::ofstream fout("shelltest/full-profile.out");
+std::vector<std::pair<std::string, std::string>> benchmarks = {
+  {"b19_iccad_eval", "b19_iccad"},
+  {"cordic_ispd2", "cordic_ispd2"},
+  {"des_perf_ispd_eval", "des_perf_ispd"},
+  {"edit_dist_ispd2", "edit_dist_ispd2"},
+  {"leon2_iccad_eval", "leon2_iccad"},
+  {"leon3mp_iccad_eval", "leon3mp_iccad"},
+  {"netcard_iccad_eval", "netcard_iccad"},
+  {"vga_lcd_iccad_eval", "vga_lcd_iccad"}
+};
+
+const int n = 2;   // number of tests for each benchmark, cpu or gpu
+
+inline void init_for(std::string name1, std::string name2, ot::Timer &timer) {
+  std::string start = "../benchmark/" + name1 + "/" + name2;
+  timer.read_celllib(start + "_Early.lib", ot::MIN)
+    .read_celllib(start + "_Late.lib", ot::MAX)
+    .read_verilog(start + ".v")
+    .read_spef(start + ".spef")
+    .read_timing(start + ".timing");
 }
 
-void i_c17(ot::Timer &timer) {
-  timer.read_celllib("./benchmark/c17/c17_Early.lib", ot::MIN)
-    .read_celllib("./benchmark/c17/c17_Late.lib", ot::MAX)
-    .read_verilog("./benchmark/c17/c17.v")
-    .read_spef("./benchmark/c17/c17.spef")
-    .read_timing("./benchmark/c17/c17.timing");
-}
+inline void test_for(std::string name1, std::string name2, bool usegpu) {
+  pid_t pid = fork();
+  if(pid == 0) {
+    fout << name1 << (usegpu ? " GPU " : " CPU ") << std::flush;
+    
+    ot::Timer timer;
+    if(usegpu) timer.cuda(true);
+    init_for(name1, name2, timer);
+    timer.update_timing();
+    size_t n_nets, n_nodes, n_pins;
+    timer.get_sizes(n_nets, n_nodes, n_pins);
 
-void i_vga_lcd(ot::Timer &timer) {
-  timer.read_celllib("./benchmark/vga_lcd/vga_lcd_Early.lib", ot::MIN)
-    .read_celllib("./benchmark/vga_lcd/vga_lcd_Late.lib", ot::MAX)
-    .read_verilog("./benchmark/vga_lcd/vga_lcd.v")
-    .read_spef("./benchmark/vga_lcd/vga_lcd.spef")
-    .read_timing("./benchmark/vga_lcd/vga_lcd.timing");
-}
+    fout << n_nets << ' ' << n_nodes << ' ' << n_pins
+         << ' ' << _prof::timers["_build_prop_cands"]
+         << ' ' << _prof::timers["_build_rc_timing_tasks"]
+         << ' ' << _prof::timers["_update_timing__taskflow_rctiming"]
+         << ' ' << _prof::timers[usegpu ? "_build_prop_tasks_cuda" : "_build_prop_tasks"]
+         << ' ' << _prof::timers["_update_timing__taskflow_prop"]
+         << ' ' << _prof::timers["_update_timing"]
+         << std::endl << std::flush;
 
-void i_wb_dma(ot::Timer &timer) {
-  timer.read_celllib("./benchmark/wb_dma/wb_dma_Early.lib", ot::MIN)
-    .read_celllib("./benchmark/wb_dma/wb_dma_Late.lib", ot::MAX)
-    .read_verilog("./benchmark/wb_dma/wb_dma.v")
-    .read_spef("./benchmark/wb_dma/wb_dma.spef")
-    .read_timing("./benchmark/wb_dma/wb_dma.timing");
-}
-
-void i_des_perf(ot::Timer &timer) {
-  timer.read_celllib("./benchmark/des_perf/des_perf_Early.lib", ot::MIN)
-    .read_celllib("./benchmark/des_perf/des_perf_Late.lib", ot::MAX)
-    .read_verilog("./benchmark/des_perf/des_perf.v")
-    .read_spef("./benchmark/des_perf/des_perf.spef")
-    .read_timing("./benchmark/des_perf/des_perf.timing");
+    exit(0);
+  }
+  else {
+    int returnStatus;
+    waitpid(pid, &returnStatus, 0);
+    std::cout << "Child process finished with exit code " << returnStatus << std::endl;
+  }
 }
 
 int main(int argc, char *argv[]) {
-  std::ofstream fout("shelltest/full-profile.out");
-  std::vector<std::pair<std::string, void (*)(ot::Timer &)>> benchmarks = {
-    {"netcard", i_netcard},      // need 20GiB memory
-    {"c17", i_c17},
-    {"vga_lcd", i_vga_lcd},
-    {"wb_dma", i_wb_dma},
-    {"des_perf", i_des_perf}
-  };
-
-  const int n = 2;   // number of tests for each benchmark, cpu or gpu
-  
-  for(auto [name, init] : benchmarks) {
+  for(auto [name1, name2] : benchmarks) {
     for(int i = 1; i <= n; ++i) {
-      std::cout << "========== " << name << " (CPU, " << i << ")" << std::endl;
-      fout << name << " CPU " << std::flush;
-      
-      ot::Timer timer;
-      init(timer);
-      timer.update_timing();
-      size_t n_nets, n_nodes, n_pins;
-      timer.get_sizes(n_nets, n_nodes, n_pins);
-
-      fout << n_nets << ' ' << n_nodes << ' ' << n_pins
-           << ' ' << _prof::timers["_build_prop_cands"]
-           << ' ' << _prof::timers["_build_rc_timing_tasks"]
-           << ' ' << _prof::timers["_update_timing__taskflow_rctiming"]
-           << ' ' << _prof::timers["_build_prop_tasks"]
-           << ' ' << _prof::timers["_update_timing__taskflow_prop"]
-           << ' ' << _prof::timers["_update_timing"]
-           << std::endl << std::flush;
+      std::cout << "========== " << name1 << " (CPU, " << i << ")" << std::endl;
+      test_for(name1, name2, false);
     }
     for(int i = 1; i <= n; ++i) {
-      std::cout << "========== " << name << " (GPU, " << i << ")" << std::endl;
-      fout << name << " GPU " << std::flush;
-      
-      ot::Timer timer;
-      timer.cuda(true);
-      init(timer);
-      timer.update_timing();
-      size_t n_nets, n_nodes, n_pins;
-      timer.get_sizes(n_nets, n_nodes, n_pins);
-
-      fout << n_nets << ' ' << n_nodes << ' ' << n_pins
-           << ' ' << _prof::timers["_build_prop_cands"]
-           << ' ' << _prof::timers["_build_rc_timing_tasks"]
-           << ' ' << _prof::timers["_update_timing__taskflow_rctiming"]
-           << ' ' << _prof::timers["_build_prop_tasks_cuda"]
-           << ' ' << _prof::timers["_update_timing__taskflow_prop"]
-           << ' ' << _prof::timers["_update_timing"]
-           << std::endl << std::flush;
+      std::cout << "========== " << name1 << " (GPU, " << i << ")" << std::endl;
+      test_for(name1, name2, true);
     }
   }
 }
