cf26286b231cca6a1ce3a6afe0b7c94510ff60b1
GZZ <gzz_2000@126.com>
2021-01-25 02:04:09 +0000

    Update local env

--- ot/config.hpp
+++ ot/config.hpp
@@ -2,7 +2,7 @@
 #define OT_VERSION_MINOR 0
 #define OT_VERSION_PATCH 0
 #define OT_VERSION "2.0.0 (alpha)"
-#define OT_HOME "/home/zizhengguo/OpenTimer"
-#define OT_HOST_SYSTEM "Linux-4.15.0-72-generic"
+#define OT_HOME "/home/zizhengguo/GPUTimer/OpenTimer"
+#define OT_HOST_SYSTEM "Linux-4.15.0-118-generic"
 #define OT_INSTALL_PREFIX "/usr/local"
-#define OT_TCLSH_PATH "/home/yibo/anaconda3/bin/tclsh"
+#define OT_TCLSH_PATH "/home/zizhengguo/.anaconda3/bin/tclsh"
